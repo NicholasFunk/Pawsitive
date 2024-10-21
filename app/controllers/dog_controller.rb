cf467a618cc32a685574f54d57271967cd533f5e
nfunk <nf4313@hotmail.com>
2024-10-21 17:38:52 -0500

    Added global variables to index, show, and new.

--- app/controllers/dog_controller.rb
+++ app/controllers/dog_controller.rb
@@ -1,10 +1,16 @@
 class DogController < ApplicationController
   def index
-    
+    @dogs = Dog.all
   end
 
   def show
+    @dog = Dog.find(params[:id])
   end
+
+  def def new
+    @dog = Dog.new
+  end
+  
 
   def edit
   end
