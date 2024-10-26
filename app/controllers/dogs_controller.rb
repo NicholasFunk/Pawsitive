1b75619b999b558e3102d7371b34d2150f03c70c
nfunk <nf4313@hotmail.com>
2024-10-26 13:52:27 -0500

    Added new dog page.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,6 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: %i[ show edit update destroy ]
+
   def index
     @dogs = Dog.includes(:dog_images, :breeds).all
   end
@@ -7,4 +9,21 @@ class DogsController < ApplicationController
     # Join on breed and image tables
     @dog = Dog.includes(:dog_images).find(params[:id])
   end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+
 end
